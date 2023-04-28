77902845fb864d7258ba13ba4c6cb70df2a8ffc3
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-04-28 03:09:41 +0200

    First Preview of InvisibleInteract

---
+++ Better268/Patches/InvisibleInteract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+
+namespace Better268.Patches;
+
+[HarmonyPatch(typeof(PlayerInteract))]
+internal static class InvisibleInteract
+{
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> OnInteract(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+        
+        newInstructions.RemoveRange(0, newInstructions.Count);
+        
+        newInstructions.Add(new CodeInstruction(OpCodes.Ret));
+
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
